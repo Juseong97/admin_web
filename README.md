8d81a947da0316076ac205ff2b4af502027630e3
Juseong97 <vnlrlvnlrl@naver.com>
2026-06-20 09:36:56 +0900

    React 프로젝트 생성

---
+++ README.md
@@ -0,0 +1,10 @@
+### ⚡ 관리자 웹페이지 
+
+- Vite + React + TypeScript 기반으로 제작된 프론트엔드 프로젝트입니다.
+## <!-- TOC -->
+    * Bulid Tool : Vite
+    * UI : React
+    * script : TypeScript
+    * css framework : Tailwind CSS
+
+<!-- TOC -->
---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from "@tailwindcss/vite";
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+      react(),
+      tailwindcss()
+  ],
+
+  //서버 포트 설정
+  server : {
+    // port : 5173,
+    strictPort : true
+  }
+})
