3c9e28f5c52daf7ddc32e4848be9fd2ba4254de5
Juseong97 <vnlrlvnlrl@naver.com>
2026-07-30 22:57:51 +0900

    intellij tailwindcss 자동완성 기능 오류 수정

--- src/pages/common/LayoutSideBar.tsx
+++ src/pages/common/LayoutSideBar.tsx
@@ -3,27 +3,40 @@ import {
     SidebarContent,
     SidebarFooter,
     SidebarHeader,
-    SidebarMenu,
+    SidebarMenu, SidebarMenuButton,
     SidebarMenuItem
 } from "@/components/ui/sidebar.tsx";
-import {User2} from "lucide-react";
-import {DropdownMenu} from "@/components/ui/dropdown-menu.tsx";
+import {ChevronDown, User2} from "lucide-react";
+import {DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuShortcut, DropdownMenuTrigger} from "@/components/ui/dropdown-menu.tsx";
 
 export default function LayoutSideBar() {
     return (
         <Sidebar variant="sidebar" side="left" collapsible="icon">
+            {/*헤더*/}
             <SidebarHeader>
                 <SidebarMenu>
                     <DropdownMenu>
-
+                        <DropdownMenuTrigger>
+                            <SidebarMenuButton>
+                                select WorkSpace
+                                <ChevronDown className={'ml-auto'}/>
+                            </SidebarMenuButton>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent className={"w-[--radix-popper-anchor-width]"}>
+                            <DropdownMenuItem>
+                                <span>Acme Inc</span>
+                                <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
+                            </DropdownMenuItem>
+                        </DropdownMenuContent>
                     </DropdownMenu>
                 </SidebarMenu>
             </SidebarHeader>
-
+            {/*컨텐츠*/}
             <SidebarContent>
 
             </SidebarContent>
 
+            {/*푸터*/}
             <SidebarFooter>
                 <SidebarMenu>
                     <SidebarMenuItem>
