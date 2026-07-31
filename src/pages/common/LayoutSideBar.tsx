7528f0fb050a501a9d996e974d9d21ccbeaa153b
Juseong97 <vnlrlvnlrl@naver.com>
2026-07-31 22:50:27 +0900

    [추가] 사이드바

--- src/pages/common/LayoutSideBar.tsx
+++ src/pages/common/LayoutSideBar.tsx
@@ -2,12 +2,15 @@ import {
     Sidebar,
     SidebarContent,
     SidebarFooter,
+    SidebarGroup,
+    SidebarGroupAction,
+    SidebarGroupLabel,
     SidebarHeader,
-    SidebarMenu, SidebarMenuButton,
+    SidebarMenu,
     SidebarMenuItem
 } from "@/components/ui/sidebar.tsx";
-import {ChevronDown, User2} from "lucide-react";
-import {DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuShortcut, DropdownMenuTrigger} from "@/components/ui/dropdown-menu.tsx";
+import {ChevronDown, Plus, User2} from "lucide-react";
+import {Collapsible, CollapsibleTrigger} from "@/components/ui/collapsible.tsx";
 
 export default function LayoutSideBar() {
     return (
@@ -15,25 +18,40 @@ export default function LayoutSideBar() {
             {/*헤더*/}
             <SidebarHeader>
                 <SidebarMenu>
-                    <DropdownMenu>
-                        <DropdownMenuTrigger>
-                            <SidebarMenuButton>
-                                select WorkSpace
-                                <ChevronDown className={'ml-auto'}/>
-                            </SidebarMenuButton>
-                        </DropdownMenuTrigger>
-                        <DropdownMenuContent className={"w-[--radix-popper-anchor-width]"}>
-                            <DropdownMenuItem>
-                                <span>Acme Inc</span>
-                                <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
-                            </DropdownMenuItem>
-                        </DropdownMenuContent>
-                    </DropdownMenu>
+                    {/*<DropdownMenu>*/}
+                    {/*    <DropdownMenuTrigger>*/}
+                    {/*        <SidebarMenuButton>*/}
+                    {/*            select WorkSpace*/}
+                    {/*            <ChevronDown className={'ml-auto'}/>*/}
+                    {/*        </SidebarMenuButton>*/}
+                    {/*    </DropdownMenuTrigger>*/}
+                    {/*    <DropdownMenuContent className={"w-[--radix-popper-anchor-width]"}>*/}
+                    {/*        <DropdownMenuItem>*/}
+                    {/*            <span>Acme Inc</span>*/}
+                    {/*            <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>*/}
+                    {/*        </DropdownMenuItem>*/}
+                    {/*    </DropdownMenuContent>*/}
+                    {/*</DropdownMenu>*/}
                 </SidebarMenu>
             </SidebarHeader>
             {/*컨텐츠*/}
             <SidebarContent>
-
+                <SidebarGroup>
+                    <SidebarGroupAction>
+                        <Plus/> <span className={'sr-only text-black'}>Add Project</span>
+                    </SidebarGroupAction>
+                </SidebarGroup>
+                <Collapsible>
+                    <SidebarGroup>
+                        <SidebarGroupLabel asChild>
+                            <CollapsibleTrigger>
+                                <ChevronDown
+                                    className={'ml-auto transition-transform group-data-[state=open]/collapsible:rotate-180'}/>
+                                홈플러스
+                            </CollapsibleTrigger>
+                        </SidebarGroupLabel>
+                    </SidebarGroup>
+                </Collapsible>
             </SidebarContent>
 
             {/*푸터*/}
